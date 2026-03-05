7d9441dc79e39e38289df1b7be4f57140700a691
Vishal Yadav <ydvishal89@gmail.com>
2026-03-05 17:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function minDate(dates) {
   //write you code here
+	let minDate = new Date(dates[0]);
+
+	for(let i =1;i<dates.length;i++){
+		let currDate = new Date(dates[i]);
+		if(currDate<minDate){
+			minDate = currDate;
+		}
+	}
+	console.log(minDate);
 }
 
 // Do not change the code
@@ -37,4 +46,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+// alert(minDate(dates));
